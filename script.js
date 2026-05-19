7bb2be18f3d707d710389a7f1e47263afc451c03
Aars <aarsgalatta@gmail.com>
2026-05-19 19:54:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-body{
-  background:#0f172a;
-  color:white;
-  text-align:center;
-  font-family:Arial;
-  padding-top:100px;
-}
+function showMessage(){
 
-button{
-  padding:10px 20px;
-  font-size:18px;
-  border:none;
-  border-radius:8px;
-  cursor:pointer;
+  alert("Welcome to the Chemistry Club!");
+
 }
