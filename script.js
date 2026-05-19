b64dd5e501e712f41faa4209f01fc1617ab57978
Aars <aarsgalatta@gmail.com>
2026-05-19 19:53:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+body{
+  background:#0f172a;
+  color:white;
+  text-align:center;
+  font-family:Arial;
+  padding-top:100px;
+}
 
+button{
+  padding:10px 20px;
+  font-size:18px;
+  border:none;
+  border-radius:8px;
+  cursor:pointer;
+}
